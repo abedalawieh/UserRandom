566abc4fdc516a4c4a6dadab8012bb45a072a8cf
abedalawieh <abedalaweye@gmail.com>
2023-09-13 22:48:01 +0300

    Added the Lazy Components

--- React/src/App.js
+++ React/src/App.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import LoginScreen from "./Screens/LoginScreen";
-import GetUsers from "./Components/GetUsers";
+// import GetUsers from "./Components/GetUsers";
 import { createContext, useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
@@ -11,9 +11,13 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import ViewAll from "./Screens/ViewAll";
-import ManuelRecords from "./Screens/ManuelRecords";
+// import ViewAll from "./Screens/ViewAll";
+import Loader from "./Components/Loader";
+// import ManuelRecords from "./Screens/ManuelRecords";
 import FirstComponent from "./Components/DateComp";
+const LazyGetUsers = React.lazy(() => import("./Components/GetUsers"));
+const LazyViewAll = React.lazy(() => import("./Screens/ViewAll"));
+const LazyManuel = React.lazy(() => import("./Screens/ManuelRecords"));
 
 export const RecoveryContext = createContext();
 
@@ -59,18 +63,53 @@ function App() {
     >
       <Router>
         <Routes>
-          <Route path="/" element={loggedin ? <ViewAll /> : <LoginScreen />} />
+          <Route
+            path="/"
+            element={
+              loggedin ? (
+                <React.Suspense fallback={<Loader />}>
+                  <LazyViewAll />{" "}
+                </React.Suspense>
+              ) : (
+                <LoginScreen />
+              )
+            }
+          />
           <Route
             path="/newUsersRequest"
-            element={loggedin ? <GetUsers /> : <LoginScreen />}
+            element={
+              loggedin ? (
+                <React.Suspense fallback={<Loader />}>
+                  <LazyGetUsers />{" "}
+                </React.Suspense>
+              ) : (
+                <LoginScreen />
+              )
+            }
           />
           <Route
             path="/login"
-            element={loggedin ? <ViewAll /> : <LoginScreen />}
+            element={
+              loggedin ? (
+                <React.Suspense fallback={<Loader />}>
+                  <LazyViewAll />{" "}
+                </React.Suspense>
+              ) : (
+                <LoginScreen />
+              )
+            }
           />
           <Route
             path="/addManuel"
-            element={loggedin ? <ManuelRecords /> : <LoginScreen />}
+            element={
+              loggedin ? (
+                <React.Suspense fallback={<Loader />}>
+                  <LazyManuel />{" "}
+                </React.Suspense>
+              ) : (
+                <LoginScreen />
+              )
+            }
           />
         </Routes>
 
